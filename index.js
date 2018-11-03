ace7b1c835b1bcf618671f0cc6cd6cbc926c1676
Maxime Mangeard <mmangear@uwo.ca>
2018-11-03 21:48:32 +0000

    last question

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var http = require('http');
+var express = require('express');
+var app = express();  
 
-var server = http.createServer(function serveRequest(request, response) {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.end("Hello World\n");
-});
+// serve files in static' folder at root URL '/'
+app.use('/', express.static('static'));
 
-server.listen(process.env.PORT, process.env.IP);
-console.log("Server at IP:" + process.env.IP + " port: "+process.env.PORT);
+app.route('/api')
+ .get(function(req, res) {
+   res.send('Got a GET request for /api');
+ })
+ .post(function(req, res) {
+   res.send('Got a POST request for /api');
+ })
+ .put(function(req, res) {
+   res.send('Got a PUT request for /api');
+ });
+
+app.listen(8080); // start server
+
+console.log('Listening on port 8080');
